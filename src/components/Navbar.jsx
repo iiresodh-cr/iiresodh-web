a5be888a34ca8d7b9f60ef94820d530910c015db
- IIRESODH - <webmaster@iiresodh.org>
2026-03-13 16:26:05 +0000

    Fix color de iconos de redes sociales

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 // src/components/Navbar.jsx
 import { Link, useLocation } from "react-router-dom";
-// Importamos el nuevo logo recortado (trim)
-import logo from "../assets/Logo_Oficiale_200w-trim.png"; 
+// Importamos el nuevo logo recortado (trim) y en formato JPG
+import logo from "../assets/Logo_Oficiale_200w-trim.jpg"; 
 
 export default function Navbar() {
   const location = useLocation();
@@ -16,7 +16,8 @@ export default function Navbar() {
             {/* Ajustamos la altura para un logo sin márgenes blancos, manteniendo coherencia */}
             <img src={logo} alt="IIRESODH Logo" className="h-16 md:h-20 w-auto object-contain" />
           </Link>
-          <div className="flex items-center gap-4 md:gap-6 text-main-blue">
+          {/* Cambiamos text-main-blue por text-light-blue para las redes sociales */}
+          <div className="flex items-center gap-4 md:gap-6 text-light-blue">
             <a href="https://www.facebook.com/iiresodhcostarica" target="_blank" rel="noreferrer" className="hover:text-main-red transition-colors">
               <svg className="w-6 h-6 md:w-7 md:h-7" fill="currentColor" viewBox="0 0 24 24"><path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" /></svg>
             </a>
